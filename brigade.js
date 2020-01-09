3d12ad8b7e78615f01c1ab9abb416089a51f13b0
janshair <janshair@ip-192-168-100-60.us-west-2.compute.internal>
2020-01-09 12:51:47 +0500

    brigade.js added.

---
+++ brigade.js
@@ -0,0 +1,8 @@
+const { events, Job } = require('@brigadecore/brigadier');
+
+events.on('exec', () => {
+  var job = new Job('do-nothing', 'alpine');
+  job.tasks = ['echo Hello', 'echo World'];
+
+  job.run();
+});
